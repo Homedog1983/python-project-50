64353405aee6a386f593bd6ee91d52ece151cb82
Evgeny Sokolov <sokolovsolod@gmail.com>
2023-05-10 21:27:59 +0700

    v 0.1.3 Add argparse -f
    Now 3 args: -f (default = ['plain']),
    positionals  'first_file', 'second_file'
    Script return all of them them as 3 print lines

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,12 +8,15 @@ def main():
     parser = argparse.ArgumentParser(
         prog='gendiff',
         description='Compares two configuration files and shows a difference.')
+    parser.add_argument('-f', '--format', nargs=1, type=str, default=['plain'],
+                        help='set format of output')
     parser.add_argument('first_file', nargs=1, type=str)
     parser.add_argument('second_file', nargs=1, type=str)
     # parser.add_argument('first_file', nargs=1, type=argparse.FileType('r'))
     # parser.add_argument('second_file', nargs=1, type=argparse.FileType('r'))
     args = parser.parse_args()
-    # print(args)
+    print(args)
+    print(f'FORMAT = {args.format[0]}')
     print(f'first_file = {args.first_file[0]}')
     print(f'second_file = {args.second_file[0]}')
 
