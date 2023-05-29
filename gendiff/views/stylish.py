0af34bec3262276dbca8208e6b91bc493813563e
Evgeny Sokolov <sokolovsolod@gmail.com>
2023-05-29 13:54:57 +0700

    v 0.7.10 'Stylish' optimizations Function's names, 2 funcs instead of 1

--- gendiff/views/stylish.py
+++ gendiff/views/stylish.py
@@ -2,7 +2,7 @@ from gendiff.diff_abstraction import is_line
 from gendiff.diff_abstraction import get_key, get_value, get_children, get_sign
 
 
-def get_formatted(value):
+def to_str(value):
     if value is None:
         return 'null'
     elif isinstance(value, bool):
@@ -10,32 +10,33 @@ def get_formatted(value):
     return str(value)
 
 
-def get_replaced(key, sign=' ', level=0, replacer=' ', replacer_per_level=4):
+def get_indents(level=0, replacer=' ', replacer_per_level=4):
     signs_to_key = replacer_per_level * level
     replacer_to_bracket = replacer * signs_to_key
     replacer_to_sign = replacer * (signs_to_key - 2)
-    replaced_key = f"{replacer_to_sign}{sign} {get_formatted(key)}"
-    replaced_bracket = f"{replacer_to_bracket}" + "}"
-    return replaced_key, replaced_bracket
+    return replacer_to_sign, replacer_to_bracket
+
+
+def add_lines_from(node, lines, level=0):
+    children = get_children(node)
+    for sub_node in children:
+        key = get_key(sub_node)
+        sign = get_sign(sub_node)
+        next_level = level + 1
+        sign_indent, bracket_indent = get_indents(next_level)
+        if is_line(sub_node):
+            value = get_value(sub_node)
+            lines.append(
+                f"{sign_indent}{sign} {to_str(key)}: {to_str(value)}")
+        else:
+            lines.append(
+                f"{sign_indent}{sign} {to_str(key)}: " + "{")
+            add_lines_from(sub_node, lines, next_level)
+            lines.append(f"{bracket_indent}" + '}')
 
 
 def stringify(tree):
     lines = ['{']
-
-    def add_lines_from(node, level=0):
-        children = get_children(node)
-        for sub_node in children:
-            key = get_key(sub_node)
-            sign = get_sign(sub_node)
-            next_level = level + 1
-            replaced_key, replaced_bracket = get_replaced(key, sign, next_level)
-            if is_line(sub_node):
-                value = get_value(sub_node)
-                lines.append(f"{replaced_key}: {get_formatted(value)}")
-            else:
-                lines.append(f"{replaced_key}: " + "{")
-                add_lines_from(sub_node, next_level)
-                lines.append(f"{replaced_bracket}")
-    add_lines_from(tree)
+    add_lines_from(tree, lines)
     lines.append('}')
     return '\n'.join(lines)
