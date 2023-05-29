641b70a9e89c04f194853264d31dfdc36d03b229
Evgeny Sokolov <sokolovsolod@gmail.com>
2023-05-29 13:09:20 +0700

    v 0.7.9 Tests optimization

--- tests/test_file_parser.py
+++ tests/test_file_parser.py
@@ -2,45 +2,33 @@ import pytest
 from gendiff.file_parser import get_dict_from_file
 
 
-def params():
-    paths = {
-        'json1': 'tests/fixtures/file1.json',
-        'json2': 'tests/fixtures/file2.json',
-        'yml1': 'tests/fixtures/file1.yml',
-        'yml2': 'tests/fixtures/file2.yml',
-        'yaml1': 'tests/fixtures/file1.yaml',
-        'yaml2': 'tests/fixtures/file2.yaml',
-        'not_exist': 'tests/fixtures/f.json',
-        'wrong_type': 'tests/fixtures/wrong_type.txt'
-    }
-
-    dict1 = {
-        "host": "hexlet.io",
-        "timeout": 50,
-        "proxy": "123.234.53.22",
-        "follow": False
-    }
-    dict2 = {
-        "timeout": 20,
-        "verbose": True,
-        "host": "hexlet.io"
-    }
-    dicts = {'1': dict1, '2': dict2, 'empty': {}}
-
-    return [
-        (paths['json1'], dicts['1']),
-        (paths['json2'], dicts['2']),
-        (paths['yml1'], dicts['1']),
-        (paths['yml2'], dicts['2']),
-        (paths['yaml1'], dicts['1']),
-        (paths['yaml2'], dicts['2']),
-        (paths['yml2'], dicts['2']),
-        (paths['yaml1'], dicts['1']),
-        (paths['wrong_type'], dicts['empty']),
-        (paths['not_exist'], dicts['empty'])
-    ]
-
-
-@pytest.mark.parametrize("path, expected_dict", params())
+@pytest.mark.parametrize("path, expected_dict", [
+    ('file1.json', 'dict_1'),
+    ('file2.json', 'dict_2'),
+    ('file1.yml', 'dict_1'),
+    ('file2.yml', 'dict_2'),
+    ('file1.yaml', 'dict_1'),
+    ('file2.yaml', 'dict_2'),
+    ('file2.yml', 'dict_2'),
+    ('file1.yaml', 'dict_1'),
+    ('wrong_type.txt', 'empty'),
+    ('f.json', 'empty')
+])
 def test_get_dict_from_file(path, expected_dict):
-    assert get_dict_from_file(path) == expected_dict
+    full_path = f'tests/fixtures/{path}'
+    if expected_dict == 'dict_1':
+        expected = {
+            "host": "hexlet.io",
+            "timeout": 50,
+            "proxy": "123.234.53.22",
+            "follow": False
+        }
+    elif expected_dict == 'dict_2':
+        expected = {
+            "timeout": 20,
+            "verbose": True,
+            "host": "hexlet.io"
+        }
+    else:
+        expected = {}
+    assert get_dict_from_file(full_path) == expected
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,76 +2,41 @@ import pytest
 from gendiff.generate_diff import get_nested_diff
 
 
-def params():
-    paths = {
-        'json1': 'tests/fixtures/file1.json',
-        'json2': 'tests/fixtures/file2.json',
-        'yml1': 'tests/fixtures/file1.yml',
-        'yml2': 'tests/fixtures/file2.yml',
-        'yaml1': 'tests/fixtures/file1.yaml',
-        'yaml2': 'tests/fixtures/file2.yaml',
-        'not_exist': 'tests/fixtures/f.json',
-        'wrong_type': 'tests/fixtures/wrong_type.txt',
-        'json1_nested': 'tests/fixtures/file1_nested.json',
-        'json2_nested': 'tests/fixtures/file2_nested.json',
-        'yaml1_nested': 'tests/fixtures/file1_nested.yaml',
-        'yaml2_nested': 'tests/fixtures/file2_nested.yaml',
-    }
-
-    diff_paths = {
-        'diff12': 'tests/fixtures/flat_diff12.txt',
-        'diff21': 'tests/fixtures/flat_diff21.txt',
-        'nested_diff12': 'tests/fixtures/nested_diff12.txt',
-        'nested_diff12_plain': 'tests/fixtures/nested_diff12_plain.txt',
-        'nested_diff12_json': 'tests/fixtures/nested_diff12_json.txt'
-    }
-
-    diffs = {}
-    with open(diff_paths['diff12']) as diff12:
-        diffs['12'] = diff12.read()
-    with open(diff_paths['diff21']) as diff21:
-        diffs['21'] = diff21.read()
-    with open(diff_paths['nested_diff12']) as nested_diff12:
-        diffs['n_s'] = nested_diff12.read()
-    with open(diff_paths['nested_diff12_plain']) as nested_diff12_plain:
-        diffs['n_p'] = nested_diff12_plain.read()
-    with open(diff_paths['nested_diff12_json']) as nested_diff12_json:
-        diffs['n_j'] = nested_diff12_json.read()
-    diffs['wrong'] = "No supported file(s). Check the types/paths of them!"
-
-    return [
-        # path1, path2, format_name, expected
-        # # wrongs -> all formats)
-        (paths['not_exist'], paths['json1'], 'sdf', diffs['wrong']),
-        (paths['wrong_type'], paths['json2'], 'plain', diffs['wrong']),
-        (paths['not_exist'], paths['json1'], 'json', diffs['wrong']),
-        (paths['wrong_type'], paths['json2'], 'stylish', diffs['wrong']),
-        # flat-json-json -> stylish
-        (paths['json1'], paths['json2'], 'stylish', diffs['12']),
-        (paths['json2'], paths['json1'], 'dfdf', diffs['21']),
-        # # flat yml-yml-yaml -> stylish
-        (paths['yml1'], paths['yml2'], 'stylish', diffs['12']),
-        (paths['yml1'], paths['yaml2'], 'sd', diffs['12']),
-        (paths['yaml2'], paths['yml1'], 'e', diffs['21']),
-        # # flat yml-json -> stylish
-        (paths['yaml1'], paths['json2'], 'stylish', diffs['12']),
-        (paths['yml2'], paths['json1'], 'sew', diffs['21']),
-        (paths['json1'], paths['yml2'], 'rtyuo', diffs['12']),
-        # # nested -> stylish
-        (paths['json1_nested'], paths['json2_nested'], 'df', diffs['n_s']),
-        (paths['yaml1_nested'], paths['yaml2_nested'], 'stylish', diffs['n_s']),
-        (paths['json1_nested'], paths['yaml2_nested'], '', diffs['n_s']),
-        # # nested -> plain
-        (paths['json1_nested'], paths['json2_nested'], 'plain', diffs['n_p']),
-        (paths['yaml1_nested'], paths['yaml2_nested'], 'plain', diffs['n_p']),
-        (paths['json1_nested'], paths['yaml2_nested'], 'plain', diffs['n_p']),
-        # # nested -> json
-        (paths['json1_nested'], paths['json2_nested'], 'json', diffs['n_j']),
-        (paths['yaml1_nested'], paths['yaml2_nested'], 'json', diffs['n_j']),
-        (paths['json1_nested'], paths['yaml2_nested'], 'json', diffs['n_j'])
-    ]
-
-
-@pytest.mark.parametrize("path1, path2, format_name, expected", params())
-def test_get_nested_diff(path1, path2, format_name, expected):
-    assert get_nested_diff(path1, path2, format_name) == expected
+@pytest.mark.parametrize("path1, path2, format_name, diff_path", [
+    # # wrongs -> all formats)
+    ('f.json', 'file1.json', 'sdf', 'wrong.txt'),
+    ('wrong_type.txt', 'file2.json', 'plain', 'wrong.txt'),
+    ('f.json', 'file1.json', 'json', 'wrong.txt'),
+    ('wrong_type.txt', 'file2.json', 'stylish', 'wrong.txt'),
+    # flat-json-json -> stylish
+    ('file1.json', 'file2.json', 'stylish', 'flat_diff12.txt'),
+    ('file2.json', 'file1.json', 'dfdf', 'flat_diff21.txt'),
+    # # flat yml-yml-yaml -> stylish
+    ('file1.yml', 'file2.yml', 'stylish', 'flat_diff12.txt'),
+    ('file1.yml', 'file2.yaml', 'sd', 'flat_diff12.txt'),
+    ('file2.yaml', 'file1.yml', 'e', 'flat_diff21.txt'),
+    # # flat yml-json -> stylish
+    ('file1.yaml', 'file2.json', 'stylish', 'flat_diff12.txt'),
+    ('file2.yaml', 'file1.json', 'sew', 'flat_diff21.txt'),
+    ('file1.json', 'file2.yaml', 'rtyuo', 'flat_diff12.txt'),
+    # # nested -> stylish
+    ('file1_n.json', 'file2_n.json', 'df', 'nested_12s.txt'),
+    ('file1_n.yaml', 'file2_n.yaml', 'stylish', 'nested_12s.txt'),
+    ('file1_n.json', 'file2_n.yaml', '', 'nested_12s.txt'),
+    # # nested -> plain
+    ('file1_n.json', 'file2_n.json', 'plain', 'nested_12p.txt'),
+    ('file1_n.yaml', 'file2_n.yaml', 'plain', 'nested_12p.txt'),
+    ('file1_n.json', 'file2_n.yaml', 'plain', 'nested_12p.txt'),
+    # # nested -> json
+    ('file1_n.json', 'file2_n.json', 'json', 'nested_12j.txt'),
+    ('file1_n.yaml', 'file2_n.yaml', 'json', 'nested_12j.txt'),
+    ('file1_n.json', 'file2_n.yaml', 'json', 'nested_12j.txt')
+])
+def test_get_nested_diff(path1, path2, format_name, diff_path):
+    fixtures = 'tests/fixtures/'
+    full_path1 = f'{fixtures}{path1}'
+    full_path2 = f'{fixtures}{path2}'
+    full_diff_path = f'{fixtures}{diff_path}'
+    with open(full_diff_path) as diff_file:
+        diff = diff_file.read()
+    assert get_nested_diff(full_path1, full_path2, format_name) == diff
